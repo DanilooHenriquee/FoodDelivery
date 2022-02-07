cfe0180fa7b0aaca8de8ebc82fc2adc6164c273f
Danilo Henrique <daniloohenriquee@gmail.com>
2022-02-07 20:43:07 -0300

    aula 31 - criando o metodo procurar no controller e model usuarios

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -4,15 +4,27 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class UsuarioModel extends Model
-{
+class UsuarioModel extends Model {
     protected $table            = 'usuarios';
     protected $returnType       = 'object';
     protected $useSoftDeletes   = true;
     protected $allowedFields    = ['nome', 'email', 'telefone'];
-    
+
     protected $useTimestamps    = true;
     protected $createdField     = 'criado_em';
     protected $updatedField     = 'atualizado_em';
     protected $deletedField     = 'deletado_em';
+
+
+    public function procurar($term) {
+
+        if ($term === null) {
+            return [];
+        }
+
+        return $this->select('id, nome')
+                    ->like('nome', $term)
+                    ->get()
+                    ->getResult();
+    }
 }
